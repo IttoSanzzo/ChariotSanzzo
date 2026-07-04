7ef5a99db1519e70e1aa226ab66bb2c121f71e5d
IttoSanzzo <konohittos@gmail.com>
2026-07-04 05:54:05 -0300

    Minor improving

--- src/Services/Implementations/DiscordBotService/Components/PresenceSentinel/PresenceResolvers/VoicePresenceResolver.cs
+++ src/Services/Implementations/DiscordBotService/Components/PresenceSentinel/PresenceResolvers/VoicePresenceResolver.cs
@@ -10,17 +10,15 @@ namespace ChariotSanzzo.Services.Components {
 			var state = await Registry.GetOrCreate(userId);
 			if (state.Get<string?>("voice.guildId") != null)
 				return state;
+			state.Set("voice.lastUpdate", DateTimeOffset.UtcNow.ToUnixTimeMilliseconds().ToString());
 			foreach (var (guildId, guild) in Client.Guilds) {
 				if (!guild.Members.TryGetValue(userId, out var member) || member.VoiceState?.ChannelId == null)
 					continue;
-				if (guild.Members.Keys.Contains(userId) && guild.Members[userId].VoiceState.ChannelId != null) {
-					state.Set("voice.lastUpdate", DateTimeOffset.UtcNow.ToUnixTimeMilliseconds().ToString());
-					state.Set("voice.guildId", guildId.ToString());
-					state.Set("voice.guildName", guild.Name);
-					state.Set("voice.channelId", guild.Members[userId].VoiceState.ChannelId.ToString()!);
-					state.Set("voice.channelName", (await guild.Members[userId].VoiceState.GetChannelAsync())!.Name);
-					return state;
-				}
+				state.Set("voice.guildId", guildId.ToString());
+				state.Set("voice.guildName", guild.Name);
+				state.Set("voice.channelId", member.VoiceState.ChannelId.ToString()!);
+				state.Set("voice.channelName", (await member.VoiceState.GetChannelAsync())!.Name);
+				return state;
 			}
 			state.Set("voice.guildId", NotFound);
 			state.Set("voice.guildName", "");
