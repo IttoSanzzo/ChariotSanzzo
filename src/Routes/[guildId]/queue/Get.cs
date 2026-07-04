01b778eeaab47791a3af06d7a91089e5f58071eb
IttoSanzzo <konohittos@gmail.com>
2026-07-04 01:31:54 -0300

    Modified Gjallar so that it's results are returned when a call is made, and added routes to ge the live services

---
+++ src/Routes/[guildId]/queue/Get.cs
@@ -0,0 +1,23 @@
+using ChariotSanzzo.Components.Gjallar;
+using ChariotSanzzo.Infrastructure.Config;
+
+namespace ChariotSanzzo.Routes {
+	file class Route() : WithFilePath(), IRoute {
+		public Delegate Handle => Handler;
+		public RouteHandlerBuilder Configure(RouteHandlerBuilder builder)
+			=> builder.WithName("Get Guild Queue Current State");
+		private static async Task<IResult> Handler(string guildId, HttpContext context) {
+			try {
+				GjallarContext gtx = new();
+				await gtx.GjallarContextUnsecureAsync(new() {
+					GuildId = guildId,
+					UserId = DiscordBotConfig.BotUserId.ToString(),
+					Command = "QueueSocketUpdateString"
+				});
+				return Results.Text((await gtx.TryCallingAsync()).Data as string, "application/json");
+			} catch {
+				return Results.Ok("");
+			}
+		}
+	}
+}
