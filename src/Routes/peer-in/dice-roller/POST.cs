e09a9573deda01b2ea0fd5829e7708316f6b1c23
IttoSanzzo <konohittos@gmail.com>
2026-09-17 22:02:41 -0300

    Updated to receive the target DiscordChatIds at a external dice roll

--- src/Routes/peer-in/dice-roller/POST.cs
+++ src/Routes/peer-in/dice-roller/POST.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using ChariotSanzzo.Components.DiceRoller;
 using DSharpPlus;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +12,13 @@ namespace ChariotSanzzo.Routes {
 			// request.Headers.TryGetValue("STP-AlbinaUserId", out var albinaUserId);
 			request.Headers.TryGetValue("STP-DiscordUserId", out var discordUserIdRaw);
 
+			ulong[] diceChannels = [];
+			if (request.Headers.TryGetValue("STP-DiscordChatIds", out var discordChatIdsRaw)) {
+				try {
+					diceChannels = JsonSerializer.Deserialize<ulong[]>(discordChatIdsRaw.ToString()) ?? [];
+				} catch (JsonException) { }
+			}
+
 			var diceExpression = dto.ToDiceExpression();
 			if (diceExpression.IsValid == false)
 				return Results.BadRequest("Invalid dice expression.");
@@ -18,10 +26,12 @@ namespace ChariotSanzzo.Routes {
 			if (!string.IsNullOrEmpty(discordUserIdRaw) && ulong.TryParse(discordUserIdRaw, out var discordUserId)) {
 				var (success, embed) = await results.ToDiscordEmbedAsync(client, discordUserId);
 				if (success) {
-					await client!.SendMessageAsync(
-						await client!.GetChannelAsync(756752942085963847),
-						embed
-					);
+					foreach (var channelId in diceChannels) {
+						_ = client!.SendMessageAsync(
+							await client!.GetChannelAsync(channelId),
+							embed
+						);
+					}
 				}
 			}
 			return Results.Ok(new { diceResults = results });
